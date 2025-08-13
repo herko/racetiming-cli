efef04c441558aeb20d6f0cadbb6361d1b9dbe7f
Lubomir Herko <lubomir@herko.sk>
2025-08-13 15:01:24 +0200

    Update RaceTimingApplication.java

--- src/RaceTimingApplication.java
+++ src/RaceTimingApplication.java
@@ -1,10 +1,19 @@
-import com.thingmagic.*;
+import com.thingmagic.*;   // RFID knižnica
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.*;
-import java.util.Map;
-import java.util.concurrent.*;
 
 public class RaceTimingApplication {
     private static final Logger logger = LoggerFactory.getLogger(RaceTimingApplication.class);
